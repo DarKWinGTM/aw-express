4990fe252666c652a53b133fa70a7d64b5e91c48
DarKWinGTM <tonmaster.1.08@hotmail.com>
2024-01-06 03:37:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,86 @@ import express from "npm:express@4.18.2";
 import { Worker } from "https://deno.land/std@0.151.0/node/cluster.ts";
 
 
-
 const app = express();
+const nodeType = Worker.isMaster ? 'Master' : 'Worker';
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Dinosaur API!");
-});
+const port = 3000;
 
-app.listen(8000);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+if (Worker.isMaster) {
+  for (let i = 0; i < 1; i++) {
+    const worker = Worker.fork();
+    worker.on('message', function (msg) {
+      console.log('Master receive msg :', msg.chat);
+      Worker.fork({
+        'waxaccount': JSON.parse(msg.chat)['waxaccount'],
+        'difficulty': JSON.parse(msg.chat)['difficulty'],
+        'lastMineTx': JSON.parse(msg.chat)['lastMineTx'],
+        'timeticket': JSON.parse(msg.chat)['timeticket'],
+      });
+    });
+  }
+
+  Worker.on('exit', (worker, code, signal) => {
+    console.log('Worker #' + worker.process.pid, 'exited', code, signal);
+    if (code !== 255) {
+      Worker.fork();
+    }
+  });
+} else {
+  if (
+    'waxaccount' in process.env &&
+    'difficulty' in process.env &&
+    'lastMineTx' in process.env &&
+    'timeticket' in process.env &&
+    process.env.waxaccount &&
+    process.env.difficulty &&
+    process.env.lastMineTx &&
+    process.env.timeticket &&
+    !process.env.waxaccount == '' &&
+    !process.env.difficulty == '' &&
+    !process.env.lastMineTx == '' &&
+    !process.env.timeticket == ''
+  ) {} else {
+
+    app.get('/', (req, res) => {
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<head>');
+      res.write('<title>now-express</title>');
+      res.write('</head>');
+      res.write('<body>');
+      res.write(`<h1>now-express ${process.pid}</h1>`);
+      res.write('</body>');
+      res.write('<html>');
+      res.end();
+    });
+
+    app.get('/trace/ip', (req, res) => {
+      fetch('https://www.cloudflare.com/cdn-cgi/trace')
+        .then((result) => result.text())
+        .then((result) => {
+          console.log(result);
+          res.setHeader('Content-Type', 'text/html');
+          res.write('<html>');
+          res.write('<head>');
+          res.write('<title>trace</title>');
+          res.write('</head>');
+          res.write('<body>');
+          res.write(`<pre>${result}</pre>`);
+          res.write('</body>');
+          res.write('<html>');
+          res.end();
+        });
+    });
+
+    app.listen(port, () => {
+      //  console.log(`Server is booming on port ${port} Visit http://localhost:${port}`);
+    });
+  }
+}
+
+
